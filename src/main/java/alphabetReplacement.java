f19771d97d63607178cc84f57764dce890e0e392
frobian21@yahoo.co.uk <frobian21@yahoo.co.uk>
2019-05-13 13:11:07 +0100

    Passed all four tests using optionals instead of nulls

--- src/main/java/alphabetReplacement.java
+++ src/main/java/alphabetReplacement.java
@@ -1,3 +1,4 @@
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -7,23 +8,24 @@ public class alphabetReplacement{
 		//stringStream.reduce((s1, s2) -> s1 + " " + s2);
 		String Answer =  stringStream.map(name -> name.toLowerCase())
 				.map(name -> replaceLetter(name))
-				.filter(x -> x!=null)
+				.filter(Optional::isPresent)
+				.map(Optional::get)
 				.collect(Collectors.joining(" "))
 				.toString();
 		System.out.println(Answer);
 		return Answer;
 	}
 	
-	public static String replaceLetter(String character) {
+	public static Optional <String> replaceLetter(String character) {
+		Optional<String> result = Optional.empty();
 		int chars = Character.codePointAt(character,0) - 96;
 		if(chars>=1 && chars<=26) {
-			return Integer.toString(Character.codePointAt(character,0) - 96);
+			result = Optional.of(Integer.toString(Character.codePointAt(character,0) - 96));
 		}
-		else
-			return null;
+		return result;
 	}
 
 	public static void main(String[] args){
-		replaceLetterWithPosition("a");
+		replaceLetterWithPosition("a s");
 	}
 }

180fbae7218de7e880e0d94c31f758d763571ac6
frobian21@yahoo.co.uk <frobian21@yahoo.co.uk>
2019-05-13 12:47:35 +0100

    Passed all four tests

--- src/main/java/alphabetReplacement.java
+++ src/main/java/alphabetReplacement.java
@@ -4,22 +4,23 @@ import java.util.stream.Stream;
 public class alphabetReplacement{
 	public static String replaceLetterWithPosition(String input) {
 		Stream<String> stringStream= input.codePoints().mapToObj(c -> String.valueOf((char) c));
-		System.out.println(Integer.toString(Character.codePointAt("a",0) - 96));
 		//stringStream.reduce((s1, s2) -> s1 + " " + s2);
 		String Answer =  stringStream.map(name -> name.toLowerCase())
 				.map(name -> replaceLetter(name))
+				.filter(x -> x!=null)
 				.collect(Collectors.joining(" "))
 				.toString();
+		System.out.println(Answer);
 		return Answer;
 	}
 	
 	public static String replaceLetter(String character) {
 		int chars = Character.codePointAt(character,0) - 96;
-		if(chars>0 && chars<=26) {
+		if(chars>=1 && chars<=26) {
 			return Integer.toString(Character.codePointAt(character,0) - 96);
 		}
 		else
-			return "";
+			return null;
 	}
 
 	public static void main(String[] args){
